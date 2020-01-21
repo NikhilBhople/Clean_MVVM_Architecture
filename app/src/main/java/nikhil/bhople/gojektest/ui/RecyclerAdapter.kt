57bc74d51121ed40ebf2ce4b1160e18a56c4726d
Nikhil Bhople <enggnikhilbhople@gmail.com>
2020-01-21 23:21:19 +0530

    expanding and collapsing recyclerview item done

--- app/src/main/java/nikhil/bhople/gojektest/ui/RecyclerAdapter.kt
+++ app/src/main/java/nikhil/bhople/gojektest/ui/RecyclerAdapter.kt
@@ -1,5 +1,7 @@
 package nikhil.bhople.gojektest.ui
 
+import android.os.Build
+import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,12 +10,20 @@ import kotlinx.android.synthetic.main.recycler_item.view.*
 import nikhil.bhople.gojektest.R
 import nikhil.bhople.gojektest.data.model.RepoResponse
 
+
 class RecyclerAdapter(val list: ArrayList<RepoResponse>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
+    private var mExpandedPosition = -1
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
         return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.recycler_item, parent, false))
 
+    }
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        this.recyclerView = recyclerView
     }
 
     override fun getItemCount(): Int {
@@ -22,6 +32,15 @@ class RecyclerAdapter(val list: ArrayList<RepoResponse>) : RecyclerView.Adapter<
 
     override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
         holder.bind(list[position])
+
+        val isExpanded = position === mExpandedPosition
+        holder.itemView.group.setVisibility(if (isExpanded) View.VISIBLE else View.GONE)
+        holder.itemView.isActivated = isExpanded
+
+        holder.itemView.setOnClickListener {
+            mExpandedPosition = if (isExpanded) -1 else position
+            notifyDataSetChanged()
+        }
     }
 
 
