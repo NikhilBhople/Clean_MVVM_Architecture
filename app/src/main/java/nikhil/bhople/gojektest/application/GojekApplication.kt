5b466717af7497b1f58414585fa5d92dcf1ca567
Nikhil Bhople <enggnikhilbhople@gmail.com>
2020-01-21 21:39:40 +0530

    injecting dependency by kodein and fetching data from server

--- app/src/main/java/nikhil/bhople/gojektest/application/GojekApplication.kt
+++ app/src/main/java/nikhil/bhople/gojektest/application/GojekApplication.kt
@@ -1,12 +1,33 @@
 package nikhil.bhople.gojektest.application
 
 import android.app.Application
+import io.reactivex.disposables.CompositeDisposable
+import nikhil.bhople.gojektest.data.api.GithubRepoInterface
+import nikhil.bhople.gojektest.data.api.GithubRepoRestClient
+import nikhil.bhople.gojektest.data.interactor.LocalDataSource
+import nikhil.bhople.gojektest.data.interactor.NetworkDataSource
+import nikhil.bhople.gojektest.ui.repository.TrendingRepoRepository
+import nikhil.bhople.gojektest.ui.repository.TrendingRepoRepositoryImpl
+import nikhil.bhople.gojektest.ui.viewmodel.TrendingRepoViewModelFactory
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
 
 class GojekApplication: Application(), KodeinAware  {
 
     override val kodein: Kodein = Kodein.lazy {
+        import(androidXModule(this@GojekApplication))
 
+        bind<GithubRepoInterface>() with singleton { GithubRepoRestClient.getClient() }
+        bind() from provider { CompositeDisposable() }
+
+        bind() from singleton { LocalDataSource() }
+        bind<NetworkDataSource>() with singleton { NetworkDataSource(instance(), instance(), instance()) }
+        bind<TrendingRepoRepository>() with singleton { TrendingRepoRepositoryImpl(instance(), instance()) }
+        bind<TrendingRepoViewModelFactory>() with provider { TrendingRepoViewModelFactory(instance()) }
     }
 }
--- app/src/main/java/nikhil/bhople/gojektest/ui/TrendingRepoActivity.kt
+++ app/src/main/java/nikhil/bhople/gojektest/ui/TrendingRepoActivity.kt
@@ -2,13 +2,41 @@ package nikhil.bhople.gojektest.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import nikhil.bhople.gojektest.R
+import nikhil.bhople.gojektest.ui.repository.TrendingRepoRepository
+import nikhil.bhople.gojektest.ui.viewmodel.TrendingRepoViewModel
+import nikhil.bhople.gojektest.ui.viewmodel.TrendingRepoViewModelFactory
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.closestKodein
+import org.kodein.di.generic.instance
 
-class TrendingRepoActivity : AppCompatActivity() {
+class TrendingRepoActivity : AppCompatActivity(), KodeinAware {
+    override val kodein: Kodein by closestKodein()
+
+    private val factory: TrendingRepoViewModelFactory by instance()
+    private val viewModel:TrendingRepoViewModel by lazy {
+        ViewModelProviders.of(this, factory).get(TrendingRepoViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trending_repo)
+
+        getData()
+    }
+
+    private fun getData() {
+        viewModel.currencyList.observe(this, Observer {
+            val idd = it
+        })
+
+        viewModel.networkState.observe(this, Observer {
+            val idd = it
+        })
     }
 
 }
